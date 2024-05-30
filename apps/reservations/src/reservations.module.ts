ad663e3100835a9c5d77821bc6400d2b3a6c13e2
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-05-30 14:35:40 +0400

    added global pipes and pino for logging

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,13 +4,17 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './entities/reservation.entity';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [DatabaseModule, DatabaseModule.forFeature([{
     name: ReservationDocument.name,
     schema: ReservationSchema
   
-  }])],
+  }]),
+  LoggerModule.forRoot()
+
+],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
