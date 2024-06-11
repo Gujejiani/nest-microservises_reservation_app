3d9c1abf6d33b5790f3901833dea0f24a634fb3c
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-06-11 19:23:57 +0400

    added users module, auth is now docerized

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DatabaseModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { UsersRepository } from './users.repository';
 
 @Module({
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  imports: [DatabaseModule, DatabaseModule.forFeature([{
+    name: UserDocument.name,
+    schema: UserSchema
+  
+  }])],
 })
 export class UsersModule {}
