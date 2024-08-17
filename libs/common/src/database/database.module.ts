596cd6965acc254269a4b3c4f6847a0372096978
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-08-17 19:03:29 +0400

    configured database module for mysql

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,22 +1,42 @@
 import { Module } from '@nestjs/common';
-import {  ConfigService } from '@nestjs/config';
-import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+// import {  ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 // import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      // imports: [ConfigModule,],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get('MONGODB_URI'),
-      }),
-      inject: [ConfigService],
+    // MongooseModule.forRootAsync({
+    //   // imports: [ConfigModule,],
+    //   useFactory: (configService: ConfigService) => ({
+    //     uri: configService.get('MONGODB_URI'),
+    //   }),
+    //   inject: [ConfigService],
 
-    }),
+    // }),
+
+    TypeOrmModule.forRootAsync({
+         useFactory: (configService: ConfigService) => ({
+          type: 'mysql',
+          host: configService.getOrThrow('MYSQL_HOST'),
+          port: configService.getOrThrow('MYSQL_PORT'),
+          database: configService.getOrThrow('MYSQL_DATABASE'),
+          username: configService.getOrThrow('MYSQL_USERNAME'),
+          password: configService.getOrThrow('MYSQL_PASSWORD'),
+          synchronize:  configService.getOrThrow('MYSQL_SYNCHRONIZE'),
+          autoLoadEntities: true,
+        }),
+        inject: [ConfigService],
+
+    })
   ],
 })
 export class DatabaseModule {
-    static forFeature(models: ModelDefinition[]){
-        return MongooseModule.forFeature(models)
+    // static forFeature(models: ModelDefinition[]){
+    //     return MongooseModule.forFeature(models)
+    // }
+    static forFeature(entities: EntityClassOrSchema[]){
+        return TypeOrmModule.forFeature(entities)
     }
 }
